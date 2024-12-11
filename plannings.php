0c21ed70b28d52e36aab47d2af38ac562abf34e7
Yoshi <andreolienzo@gmail.com>
2024-12-11 09:20:19 +0100

    UPDATE BOOTSTRAP VERSION

--- plannings.php
+++ plannings.php
@@ -7,10 +7,10 @@
     <meta charset="UTF-8">
     <title>StudentFest 2024 - Plannings Détaillés</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.3/js/bootstrap.min.js"></script>
     <script>
         // Script JavaScript de base
         document.addEventListener('DOMContentLoaded', () => {
